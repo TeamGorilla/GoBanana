4529d5d94b4f68b24541e056ebb028a568973517
uduse <i@uduse.com>
2020-03-04 00:38:49 -0700

    Make Fei happy (use better practice). Make Yoni happy (use "evaluators" instead of "agents")

--- gobanana/comm/comm.py
+++ gobanana/comm/comm.py
@@ -9,21 +9,25 @@ def get_client():
 
 class TaskDispatcher(object):
     def __init__(self, task: 'comm.Task'):
-        self.task = task
-        self.client = get_client()
-        self.pubsub = self.client.pubsub()
+        self._task = task
+        self._client = get_client()
+        self._pubsub = self._client.pubsub()
+
+    @property
+    def task(self):
+        return self._task
 
     def send(self):
-        self.pubsub.subscribe(self.task.response_channel)
+        self._pubsub.subscribe(self._task.response_channel)
 
         # eat the subscription confirmation message
-        self.pubsub.get_message()
+        self._pubsub.get_message()
 
-        self.client.publish(self.task.request_channel, self.task.serialize())
+        self._client.publish(self._task.request_channel, self._task.serialize())
 
     def wait(self):
         while True:
-            message = self.pubsub.get_message()
+            message = self._pubsub.get_message()
             if message['type'] in ('message', 'pmessage'):
                 return comm.Task.deserialize(message['data'])
 
@@ -31,15 +35,15 @@ class TaskDispatcher(object):
 class TaskWorker(object):
     def __init__(self):
         # self.channel_pattern = channel_pattern
-        self.client = get_client()
-        self.pubsub = self.client.pubsub()
-        self.pubsub.psubscribe(comm.channels.TASK_REQUEST_PATTERN)
+        self._client = get_client()
+        self._pubsub = self._client.pubsub()
+        self._pubsub.psubscribe(comm.channels.TASK_REQUEST_PATTERN)
 
     def wait(self):
         while True:
-            message = self.pubsub.get_message()
+            message = self._pubsub.get_message()
             if message['type'] in ('message', 'pmessage'):
                 return comm.TaskResponse.deserialize(message['data'])
 
     def send(self, response: 'comm.TaskResponse'):
-        self.client.publish(response.task.response_channel, response.serialize())
+        self._client.publish(response.task.response_channel, response.serialize())
