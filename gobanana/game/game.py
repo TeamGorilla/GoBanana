4f733f5bdd17dd24a241985bdfd6df4bdc8ec765
DEMcKnight <dem@ou.edu>
2020-03-08 16:09:04 -0600

    Updating game.py to not hold player position - that gets passed around

--- gobanana/game/game.py
+++ gobanana/game/game.py
@@ -16,15 +16,13 @@ class Game():
 		player (Tuple<int>): The player's cordinates within the board (i.e., (0,0) is the upper-left corner; (0,1) is one spot right.)
 	"""
 	
-	def __init__(self, board, player:Tuple = (0,0)):
+	def __init__(self, board):
 		assert len(board.shape) == 2, f"The board's dimensionality should be 2. Instead it was {len(board.shape)}"
 		assert board.dtype == np.int, f"The elements of the board should be integers. Instead they were {board.dtype}"
 		assert board.min() >= min(Tiles), f"There are invalid tiles in the given board"
 		assert board.max() <= max(Tiles), f"There are invalid tiles in the given board"
 		assert all(dim>0 for dim in board.shape), f"Board dimensions must be greater than 0. Board dimensions were {self._board.shape}"
-		assert board[player] is not Tiles.WALL, f"Player cannot start in a wall. Player position was {self._player}"
 		self._board = board
-		self._player = player
 
 	@property
 	def board(self):
@@ -32,83 +30,78 @@ class Game():
 		return copy.deepcopy(self._board)
 
 	@property
-	def player(self):
-		"""The player position atop the board"""
-		return copy.deepcopy(self._player)
-
-	@property
 	def shape(self):
 		"""The dimensions of the board"""
 		return copy.deepcopy(self._board.shape)
 
-	def is_on_board(self, newposition):
+	def is_on_board(self, position):
 		"""Returns true if the given position is on the board"""
-		assert len(self.shape) == len(newposition), f"Can only check to see if the newposition is on the board if newposition has the same number of dimensions as the board"
-		return all(0<=newposition[i]<self.shape[i] for i in range(len(newposition)))
+		assert len(self.shape) == len(position), f"Can only check to see if the newposition is on the board if newposition has the same number of dimensions as the board"
+		return all(0<=position[i]<self.shape[i] for i in range(len(position)))
 
-	def is_residable(self, newposition):
+	def is_residable(self, position):
 		"""Returns true if the given location is both on the board and not a wall tile (i.e. a valid location for the player to exist)"""
-		assert len(self.shape) == len(newposition), f"Can only check to see if the newposition is on the board if newposition has the same number of dimensions as the board"
+		assert len(self.shape) == len(position), f"Can only check to see if the position is on the board if position has the same number of dimensions as the board"
 		#Check whether the new position is on the board and is not a wall tile
-		if not self.is_on_board(newposition):
+		if not self.is_on_board(position):
 			return False
-		if self.board[newposition] == Tiles.WALL:
+		if self.board[position] == Tiles.WALL:
 			return False
 		return True
 
-	def is_movable_direction(self, direction):
+	def is_movable_direction(self, position, direction):
 		"""
 		Returns true if the player can take a step in a given direction, where the direction is a tuple
 		representing the change in coofrom board import Boarddinates from the current position
 		"""
-		newposition = add_tuples(self._player, direction)
+		newposition = add_tuples(position, direction)
 		return self.is_residable(newposition)
 
-	def moved_player(self, direction):
+	def moved_player(self, position, direction):
 		"""Returns the position to which a player would reside were they to move."""
 		assert direction in Directions, f"The direction should be one of the directions in {list(Directions)}"
-		assert self.is_movable_direction(direction), f"Player cannot move in the provided direction, {Directions(direction)}:{direction}."
+		assert self.is_movable_direction(position, direction), f"Player cannot move in the provided direction, {Directions(direction)}:{direction}."
 
 		#The first step prior to banana-slipping
-		newposition = add_tuples(self._player, direction)
+		newposition = add_tuples(position, direction)
 		#If that step was a banana peel, or they started on a banana, keep moving the player until they hit an invalid position
-		if self.board[newposition] == Tiles.BANANA_PEEL or self.board[self._player] == Tiles.BANANA_PEEL:
+		if self.board[newposition] == Tiles.BANANA_PEEL or self.board[position] == Tiles.BANANA_PEEL:
 			nextposition = add_tuples(newposition, direction)
 			while self.is_residable(nextposition):
 				newposition = nextposition
 				nextposition = add_tuples(newposition, direction)
 		#Return the final valid position
-		return newposition
+		return newposition	
 
-	def child_player_positions(self):
+	def child_player_positions(self, position):
 		"""Returns the possible player positions to which a player may move from this point"""
 		#Check each direction the player can move in and append the position resulting from that movement to a list
 		attainable_positions = []
 		for direction in Directions:
-			if self.is_movable_direction(direction):
-				newposition = self.moved_player(direction)
+			if self.is_movable_direction(position, direction):
+				newposition = self.moved_player(position, direction)
 				attainable_positions.append(newposition)
 		#Return the list of player positions resulting from valid movements
 		return attainable_positions
 
-	def child_games(self):
+	def child_games(self, position):
 		"""Returns the possible games that result from a player move from this point"""
 		#Take each position resulting from valid movements and create a new corresponding game object
 		available_games = []
-		for child_position in self.child_player_positions():
+		for child_position in self.child_player_positions(position):
 			available_games.append(Game(self._board, child_position))
 		#Return the list of available 
 		return available_games
 
-	def is_won(self):
+	def is_won(self, position):
 		"""Returns true if the game has been won (the bottom-right corner has been reached)"""
-		return self._player == add_tuples(self.shape, (-1, -1))
+		return position == add_tuples(self.shape, (-1, -1))
 	
-	def __str__(self):
+	def __str__(self, position):
 		"""Represents the game using ascii characters"""
 		vectorized_converter = np.vectorize(lambda x: Tiles(x).character)
 		convertedboard = vectorized_converter(self._board)
-		convertedboard[self._player] = '@'
+		convertedboard[position] = '@'
 		return '\n'.join(''.join(row) for row in convertedboard)
 
 	@staticmethod
@@ -118,4 +111,4 @@ class Game():
 		assert len(dims)==2, f"The game board should only have width and height. Dims provided: {dims}."
 		board = np.random.choice(a=Tiles, size=dims)
 		board[(0,0)] = Tiles.FLOOR
-		return Game(board, (0,0))
+		return Game(board)
