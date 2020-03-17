042086b8420d3045fff252b1470df3e44ee2cfff
Fei Wang <tellfei@hotmail.com>
2020-03-16 23:04:02 -0600

    Evaluator should be abstract class

--- gobanana/nn/evaluator.py
+++ gobanana/nn/evaluator.py
@@ -8,16 +8,5 @@ class Evaluator(nn.Module):
         self.board_shape = board_shape
         self.num_metrics = num_metrics
         
-        # define layers, currently, this is a simple NN with two hidden layers
-        self.fc1 = nn.Linear(board_shape, 2 * num_metrics)
-        self.fc2 = nn.Linear(2 * num_metrics, num_metrics)
-        
-
     def forward(self, one_hot_board):
-        
-        # input to first FC layer, use Relu as activation function 
-        x = F.relu(self.fc1(one_hot_board))
-        # compute output layer
-        x = self.fc2(x)
-        
-        return (x)
+        pass
